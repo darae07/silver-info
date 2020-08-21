d90160bd410e76f8cd3b6c00bfb325adb670fde0
pingu9 <h1h2h1@naver.com>
2020-08-22 00:53:21 +0900

    initial server setting
    향후 작업을 위한 초기 파일입니다.

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const userRouter = require('./routes/users');
+const cors = require('cors');
+
+const app = express();
+const port = 8080;
+
+let corsOpt = {
+    origin: [
+        'http://localhost:3000',
+    ],
+    methods: ['POST', 'GET', 'DELETE', 'PUT'],
+    credentials: true,
+};
+app.use(express.json());
+app.use(cors(corsOpt));
+app.use(
+    session({
+        secret: 's!lve2r!',
+        resave: false,
+        cookie: { maxAge: 3600000 },
+        rolling: true,
+        saveUninitialized: true,
+    })
+);
+app.use(cookieParser());
+
+const root = '/';
+
+app.get(root, (req, res) => {
+    res.status(200).end('hi!');
+});
+
+app.use('/users', userRouter);
+
+app.set('port', port);
+app.listen(app.get('port'), () => {
+    console.log(`app is listening in PORT ${app.get('port')}`);
+});
+
+module.exports = app;
